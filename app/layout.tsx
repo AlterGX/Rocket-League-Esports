91b18726bb1744782868aa0b82fdedd8ab340a7e
AlterGX <leonardo.j.redes@gmail.com>
2026-05-04 09:40:47 -0400

    Una T por la r De Market

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'RL26 - Marker | La Tabla Renovada',
+  title: 'RL26 - Market | La Tabla Renovada',
   description: 'La Tabla ahora renovada muestra estadisticas, ofertas y premios',
   keywords: ['Rocket League', 'E-Sports', 'Market', 'RL26', 'Gaming'],
 }
